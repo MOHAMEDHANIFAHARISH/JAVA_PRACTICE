b11af562ef80d304ae3412d4c767330fc8837876
MOHAMED HANIFA HARISH <89599412+MOHAMEDHANIFAHARISH@users.noreply.github.com>
2026-01-28 11:50:00 +0530

    change

---
+++ JavaPractice/src/practice/FunctionWithOutObject.java
@@ -0,0 +1,16 @@
+package practice;
+
+
+
+public class FunctionWithOutObject {
+	
+	 static void Func1() {
+		System.out.println("Hello");
+	};
+
+	public static void main(String[] args) {
+		Func1();
+
+	}
+
+}
