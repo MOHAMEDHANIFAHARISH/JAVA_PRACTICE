9a8a4469f41d6774fc7ec9f109426f5d214fdd93
MOHAMED HANIFA HARISH <89599412+MOHAMEDHANIFAHARISH@users.noreply.github.com>
2026-01-29 18:05:12 +0530

    change

---
+++ JavaPractice/src/practice/Static.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Static {
+	
+	static void fun() {
+		System.out.println("hello");
+	}
+	
+	 class main {
+	  
+	  public static void main(String[] arg
+
+	 }
+	 }
+
+}
